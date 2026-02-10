d9b2ab4b6af9702b6e069a1188d6225f1baabffe
xjdsld <generalmailbox@ukr.net>
2026-02-10 15:56:41 +0200

    Refactor Database class for improved connection handling
    Refactor Database class to initialize connection and cursor in the constructor. Update create_db method to use instance variables.

--- database.py
+++ database.py
@@ -1,12 +1,25 @@
 import sqlite3
 
 class Database:
-    def __init__(self, database):
+    def __init__(self):
         self.database = database
+        self.connection = sqlite3.connect('database.db')
+        self.cursor = connection.cursor()
 
     def create_db(self):
-        connection = sqlite3.connect('database.db')
-        cursor = connection.cursor()
-        cursor.execute("""CREATE TABLE IF NOT EXISTS words (id INTEGER PRIMARY KEY AUTOINCREMENT,word TEXT,translation TEXT, sentence TEXT)""")
-        connection.commit()
-        connection.close()
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS words (id INTEGER PRIMARY KEY AUTOINCREMENT,word TEXT,translation TEXT, sentence TEXT)""")
+        self.connection.commit()
+
+    def create_card(self):
+        self.cursor.execute("SELECT word, translation, sentence FROM words")
+        row = self.cursor.fetchone()
+        if row is None:
+            print("No words to learn")
+            return None
+            
+        word = row[0]
+        translation = row[1]
+        sentence = row[2]
+        return Card(word, translation, sentence)
+
+        
