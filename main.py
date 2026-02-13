93d2021c402e14b676b708e47bbd5c3fe86cc408
xjdsld <generalmailbox@ukr.net>
2026-02-13 14:19:25 +0200

    Add main menu function for user interaction

---
+++ main.py
@@ -0,0 +1,14 @@
+def menu():
+    print("======Welcome to the Main Menu=======")
+    print("To start a new learning session press - 1")
+    print("To add a new word press - 2")
+    user_choice = int(input("Select how you want to proceede"))
+    try:
+       if user_choice == 1:
+           pass
+       elif user_choice == 2:
+           database.create_card()
+       else:
+           print("Please enter either 1 or 2")
+    except ValueError:
+        print("Please enter a number")
