3e5e8a8e14368cf0d9c5aac142af95ae32bd408a
xjdsld <generalmailbox@ukr.net>
2026-02-21 17:09:02 +0200

    Update logging.py

--- logging.py
+++ logging.py
@@ -1,2 +1,8 @@
 import logging
 
+logging.basicConfig(format='%(asctime)s - '
+                            file
+                           '%(name)s - '
+                           '%(levelname)s - '
+                           '%(message)s',
+                    level=logging.INFO)
