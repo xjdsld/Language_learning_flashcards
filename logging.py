6aa9aee7427c066011ed3e8c5fef29cc501c027b
xjdsld <generalmailbox@ukr.net>
2026-02-25 09:38:38 +0200

    Update and rename logging.py to logging_set.py

--- logging.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logging.basicConfig(format='%(asctime)s - '
-                            file
-                           '%(name)s - '
-                           '%(levelname)s - '
-                           '%(message)s',
-                    level=logging.INFO)
