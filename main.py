e314b0ea7bd09408bf876ad98272fd8966fcbd58
xjdsld <generalmailbox@ukr.net>
2026-02-26 20:13:45 +0200

    Enhance menu functionality with new features
    Refactor menu function to improve user interaction and add exit option.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging_set
+import logging
 import random
 from database import Database
 from session import Card
@@ -10,41 +11,58 @@ logger = logging.getLogger(__name__)
 def menu():
     database = Database()
     database.create_db()
-    translation = Translation()
-    print("======Welcome to the Main Menu=======")
-    print("To start a new learning session press - 1")
-    print("To add a new word press - 2")
-    user_choice = int(input("Select how you want to proceede"))
-    logger.info(f"User selected {user_choice}")
-    try:
-       if user_choice == 1:
-            cards = database.get_all_cards()
-            if not cards:
-                print("No words to show")
-                
-            random.shuffle(cards)
-            print("======NEW LEARNING SESSION======")
-            for i, card in cards enumerate(cards, start=1)
-                print(f"Word: {card.word}")
-                input("Press Enter to reveal translation...")
-                print(f"Translation: {card.translation}")
-                print(f"Example: {card.sentence}")
-                logger.info(card.word, card.translation)
-       elif user_choice == 2:
-                word = input("Enter English word: ")
-                logger.info(f"Addd a new word {word}")
-                translation = translation.translate(word)
-                sentence = input("Enter example sentence: ")
-                card = Card(word, translation, sentence)
-                database.add_card(card)
-                print("Card added successfully.")
+    translator = Translation()
 
-       else:
-           print("Please enter either 1 or 2")
-           logger.info(f"Invalid choice : {user_choice}")
-    except ValueError:
-        print("Please enter a number")
-        logger.warning(f"Invalid choice: {user_choice}")
+    while True:
+        print("======Welcome to the Main Menu=======")
+        print("To start a new learning session press - 1")
+        print("To add a new word press - 2")
+        print("0 - Exit")
+
+        try:
+           user_choice = int(input("Select how you want to proceed:"))
+           logger.info(f"User selected {user_choice}")
+           if user_choice == 1:
+                cards = database.get_all_cards()
+                if not cards:
+                    print("No words to show")
+
+                random.shuffle(cards)
+                print("======NEW LEARNING SESSION======")
+
+
+                for i, card in enumerate(cards, start=1):
+                    print(f"Word: {card.word}")
+                    user_input = input("Press Enter to reveal translation or 'q' to quit: ").lower()
+                    if user_input == "q":
+                        print("Exiting learning session...")
+                        break
+                    input("Press Enter to reveal translation...")
+                    print(f"Translation: {card.translation}")
+                    print(f"Example: {card.sentence}")
+           elif user_choice == 2:
+               while True:
+                    word = input("Enter English word (press 'f' to exit): ").lower()
+                    if word == "f":
+                        print('Back to main menu')
+                        break
+                    logger.info(f"Addd a new word {word}")
+                    translated_word = translator.translation(word)
+                    sentence = input("Enter example sentence: ")
+                    card = Card(word, translated_word, sentence)
+                    database.add_card(card)
+                    print("Card added successfully.")
+
+           elif user_choice == 0:
+               print("Exiting...")
+               break
+
+           else:
+               print("Please enter either 1 or 2")
+               logger.info(f"Invalid choice : {user_choice}")
+        except ValueError:
+            print("Please enter a number")
+            logger.warning(f"Invalid choice: {user_choice}")
 
 if __name__ == "__main__":
     menu()
