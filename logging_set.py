72403eac86bc60cd63033f34b7970742f4ed2d35
xjdsld <generalmailbox@ukr.net>
2026-03-03 21:27:17 +0200

    Add UTF-8 encoding to log file handler

--- logging_set.py
+++ logging_set.py
@@ -1,3 +1,3 @@
 import logging
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', handlers=[logging.FileHandler("app.log")])
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', handlers=[logging.FileHandler("app.log" , encoding="utf-8")])
