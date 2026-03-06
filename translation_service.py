59b89ee1536e1c3bfa997a36c321997745fbcebd
xjdsld <generalmailbox@ukr.net>
2026-03-06 16:36:14 +0200

    Refactor comment for clarity in translation_service.py

--- translation_service.py
+++ translation_service.py
@@ -17,4 +17,5 @@ class TranslationService():
          data = response.json()
          return data["responseData"]["translatedText"]
 
-# requests.get returns a http object and in data = response.json() we convert the json to a python nested dictionary. So ["responseData"] and ["translatedText"] are two dictionary keys to get the value ["translatedText"]. so
+# requests.get returns a http object and in data = response.json() we convert the json to a python nested dictionary. 
+So ["responseData"] and ["translatedText"] are two dictionary keys to get the value ["translatedText"]. so
